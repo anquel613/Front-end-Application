9f2b2646de5f9f500d7174ba87b96f23ab6d84f3
Miguel Angel Daza Buitrago <anquel613@gmail.com>
2020-11-15 18:22:29 -0500

    initial commit

---
+++ src/app/clientes/cliente.ts
@@ -0,0 +1,7 @@
+export class Cliente {
+  id: number;
+  fullname: string;
+  birth: string;
+  fatherId: number;
+  motherId: number;
+}
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,6 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, fullname: 'Miguel', birth: '2017-12-11', fatherId: 0, motherId: 0   },
+  {id: 2, fullname: 'Andres', birth: '2018-12-11', fatherId: 0, motherId: 0   }
+]
